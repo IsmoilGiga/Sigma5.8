746ad580a14b46dd27594222549aadb3b032196e
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-05-31 11:47:49 +0500

    Sigma5.8

---
+++ index.js
@@ -0,0 +1,13 @@
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let h1 = document.querySelector("h1");
+
+plus.addEventListener("click", () => {
+ h1.textContent = Number(h1.textContent) + 1;
+ h1.style = "color: green"
+});
+
+minus.addEventListener("click", () => {
+    h1.textContent = Number(h1.textContent) - 1;
+    h1.style = "color: red"
+   });
